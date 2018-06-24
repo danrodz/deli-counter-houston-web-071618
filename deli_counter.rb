2822dc69b607079e893ca82d1af9e7687cddd826
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-24 20:45:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,4 +5,4 @@ katz_deli = []
 def line(katz_deli)
   return "The line is currently empty" if katz_deli.count == 0
   sentence = "The line is currently: "
-  katz_deli.each.with_index 
+  katz_deli.each.with_index(1)
