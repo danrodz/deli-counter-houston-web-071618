cb1f1a002558aa28277b9157bc4ac171833677ef
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-24 20:51:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@
 katz_deli = []
 
 def line(katz_deli)
-  return "The line is currently empty." if katz_deli.length == 0
+  return "The line is currently empty." 
+  if katz_deli.length == 0
   sentence = "The line is currently:"
   katz_deli.each.with_index(1) do |customer, number|
     sentence += " #{number}. #{customer}"
