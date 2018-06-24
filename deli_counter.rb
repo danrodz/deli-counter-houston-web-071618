68a09f3ba5b33ad3e9ca9d8d359cfc8738af29cf
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-24 21:06:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 
 def line(array)
   sentence = "The line is currently empty."
-  if array.count > 0
+  if !array.empty
     sentence = "The line is currently:"
     array.each.with_index(1) do |customer, index|
       sentence += " #{index}. #{customer}"
